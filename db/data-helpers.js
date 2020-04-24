8a7d01a05a178c78f87f01d2e2c31dc7d3ae6927
Eli Zevin <dizzyjaguar@gmail.com>
2020-04-24 10:48:15 -0700

    create data-helpers and seed

---
+++ db/data-helpers.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const connect = require('../lib/utils/connect');
+const seed = require('../db/seed');
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+beforeAll(() => {
+  connect();
+});
+
+beforeEach(() => {
+  return mongoose.connection.dropDatabase();
+});
+
+beforeEach(() => {
+  return seed();
+});
+
+afterAll(() => {
+  return mongoose.connection.close();
+});
+
+const prepare = model => JSON.parse(JSON.stringify(model));
+const prepareAll = models => models.map(prepare);
+
+//read the models 
+const files = fs.readdirSync('./lib/models');
+const getters = files
+  //for each model import the model
+  .map(file => require(`../lib/models/${file}`))
+  //double check to make sure its a model
+  .filter(Model => Model.prototype instanceof mongoose.Model)
+  //for each model create a getModelName function that returns an instance of the model
+  .reduce((acc, Model) => {
+    return {
+      ...acc,
+      [`get${Model.modelName}`]: (query, select) => Model.findOne(query).select(select).then(prepare),
+      [`get${Model.modelName}s`]: (query, select) => Model.find(query).select(select).then(prepareAll)
+    };
+  }, {});
+
+module.exports = getters;
